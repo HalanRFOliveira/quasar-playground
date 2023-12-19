46c96a629417ab3b2d24ec41b567b6986e7b778b
AzureAD\HalanRobsonFerreirad <holiveira@deserv.com.br>
2023-12-19 17:12:17 -0300

    Added temporary layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,30 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh Lpr fff">
     <q-header elevated>
       <q-toolbar>
         <q-btn
           flat
           dense
           round
-          icon="menu"
+          :icon="leftDrawerOpen ? 'menu_open' : 'menu'"
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Halan's Projects </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-tabs>
+          <q-tab name="Produtos" label="Produtos" />
+          <q-tab name="Treinamentos" label="Treinamentos" />
+          <q-tab name="Sobre" label="Sobre" />
+          <q-tab name="Contatos" label="Contatos" />
+        </q-tabs>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -42,75 +37,66 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer
+      ><q-tabs>
+        <q-tab name="whatsapp" icon="fa-brands fa-whatsapp" label="WhatsApp" />
+        <q-tab
+          name="instagram"
+          icon="fa-brands fa-instagram"
+          label="Instagram"
+        />
+        <q-tab
+          name="youTube"
+          icon="fa-brands fa-youtube"
+          label="YouTube"
+        /> </q-tabs
+    ></q-footer>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Github",
+    caption: "github.com/HalanRFOliveira",
+    icon: "fa-brands fa-github",
+    link: "https://github.com/HalanRFOliveira",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "LinkedIn",
+    caption: "in/oliveirahalan",
+    icon: "fa-brands fa-linkedin",
+    link: "https://www.linkedin.com/in/oliveirahalan",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Instagram",
+    caption: "instagram/halan_oliveira",
+    icon: "fa-brands fa-instagram",
+    link: "https://www.instagram.com/halan_oliveira",
   },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-    EssentialLink
+    EssentialLink,
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="flex flex-center">
     <img
+    class="img"
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      src="https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2023/10/22/1147024752-e558c1c632c66325505ab13f4e437cdf.jpg"
     >
   </q-page>
 </template>
@@ -15,3 +15,10 @@ export default defineComponent({
   name: 'IndexPage'
 })
 </script>
+<style scoped>
+.img {
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+}
+</style>
